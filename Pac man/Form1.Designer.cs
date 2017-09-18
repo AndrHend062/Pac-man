bbeac82ece5ea85fe39a04d07d3e0e44ab858ad1
temp <temp>
2017-09-18 12:53:46 -0400

    Add project files.

---
+++ Pac man/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Pac_man
+{
+    partial class pacMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.startButton.ForeColor = System.Drawing.Color.DarkBlue;
+            this.startButton.Location = new System.Drawing.Point(195, 318);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(158, 45);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pacMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(592, 375);
+            this.Controls.Add(this.startButton);
+            this.Name = "pacMan";
+            this.Text = "Pac Man";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+    }
+}
+
---
+++ Pac man/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace Pac_man
+{
+    public partial class pacMan : Form
+    {
+        public pacMan()
+        {
+            InitializeComponent();
+        }
+      
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startButton.Visible = false;
+            
+
+            Graphics formGraphics = this.CreateGraphics();// graphics setup
+            Pen drawPen = new Pen(Color.Blue, 10);
+            SolidBrush drawBrush = new SolidBrush(Color.Yellow);
+            SolidBrush drawBrush2 = new SolidBrush(Color.Black);
+            Font drawFont = new Font("Arial", 16, FontStyle.Bold);
+
+            formGraphics.DrawLine(drawPen, 0, 20, 400, 20); // top line 
+            formGraphics.DrawLine(drawPen, 0, 200, 200, 200);
+            formGraphics.DrawLine(drawPen, 400, 20, 400, 300);
+            formGraphics.DrawLine(drawPen, 200, 200, 200, 300);
+            Thread.Sleep(500);
+            formGraphics.FillPie(drawBrush2, 250, 200, 50, 50, 120, 300);
+
+
+
+
+            formGraphics.FillPie(drawBrush,50,50,50,50,30,300);
+            Thread.Sleep(500);
+            formGraphics.FillPie(drawBrush2, 50, 50, 50, 50, 30, 300);
+            formGraphics.FillPie(drawBrush, 100, 50, 50, 50, 30, 360);
+            Thread.Sleep(500);
+            formGraphics.FillPie(drawBrush2, 100, 50, 50, 50, 30, 360);
+            formGraphics.FillPie(drawBrush, 150, 50, 50, 50, 30, 300);
+            Thread.Sleep(500);
+            formGraphics.FillPie(drawBrush2, 150, 50, 50, 50, 30, 360);
+            formGraphics.FillPie(drawBrush, 200, 50, 50, 50, 30, 360);
+            Thread.Sleep(500);
+            formGraphics.FillPie(drawBrush2, 200, 50, 50, 50, 30, 360);
+            formGraphics.FillPie(drawBrush, 250, 50, 50, 50, 30, 300);
+            Thread.Sleep(500);
+            formGraphics.FillPie(drawBrush2, 250, 50, 50, 50, 30, 360);
+            formGraphics.FillPie(drawBrush, 250, 50, 50, 50, 120, 360);
+            Thread.Sleep(500);
+            formGraphics.FillPie(drawBrush2, 250, 50, 50, 50, 120, 360);
+            formGraphics.FillPie(drawBrush, 250, 100, 50, 50, 120, 300);
+            Thread.Sleep(500);
+            formGraphics.FillPie(drawBrush2, 250, 100, 50, 50, 120, 360);
+            formGraphics.FillPie(drawBrush, 250, 150, 50, 50, 120, 360);
+            Thread.Sleep(500);
+            formGraphics.FillPie(drawBrush2, 250, 150, 50, 50, 120, 360);
+            formGraphics.FillPie(drawBrush, 250, 200, 50, 50, 120, 300);
+
+            formGraphics.DrawString("Congratulations", drawFont, drawBrush, 50, 60);
+            Thread.Sleep(1000);
+            startButton.Visible = true;
+
+        }
+    }
+}
